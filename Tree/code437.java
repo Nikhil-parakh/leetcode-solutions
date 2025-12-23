1c3712c4011049d911f9eb094b8887782ed7c471
Nikhil-parakh <nickparakh098@gmail.com>
2025-12-23 10:46:14 +0000

    question 437 done

---
+++ Tree/code437.java
@@ -0,0 +1,68 @@
+/* Leetcode 437 => PathSum 3
+   
+*/
+
+import java.util.*;
+
+class Node{
+    Node left;
+    Node right;
+    int val;
+
+    Node(int val){
+        this.left = null;
+        this.right = null;
+        this.val = val;
+    }
+}
+
+
+
+public class code437{
+
+    public static int helper(Node root, long sum){
+        if(root == null) return 0;
+        int count = 0;
+        if(root.val == sum) count++;
+        count += helper(root.left, sum-root.val) + helper(root.right, sum-root.val);
+        return count;
+    }
+
+    public static int pathSum(Node root, int targetSum) {
+        if(root == null) return 0;
+        return helper(root, targetSum) + pathSum(root.left, targetSum) + pathSum(root.right, targetSum);
+    }
+
+    public static void preOrder(Node root){
+        if(root == null) return;
+        System.out.print(root.val+" ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    public static void main(String args[]){
+        Node a = new Node(10);
+        Node b = new Node(5);
+        Node c = new Node(-3);
+        Node d = new Node(3);
+        Node e = new Node(2);
+        Node f = new Node(11);
+        Node g = new Node(3);
+        Node h = new Node(-2);
+        Node i = new Node(1);
+
+        a.left = b;
+        a.right = c;
+        b.left = d;
+        b.right = e;
+        c.right = f;
+        d.left = g;
+        d.right = h;
+        e.right = i;
+
+        // preOrder(a);
+        // System.out.println();
+
+        System.out.print(pathSum(a, 8));
+    }
+}
