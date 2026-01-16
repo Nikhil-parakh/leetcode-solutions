9ad879b86be80e2c4ec783c2b930400f6d2285ca
Nikhil-parakh <nickparakh098@gmail.com>
2026-01-16 10:36:24 +0000

    question 2744 done

--- HashMap&Set/code2442.java
+++ HashMap&Set/code2442.java
@@ -2,25 +2,23 @@
 import java.util.HashSet;
 public class code2442{
     public static void main(String args[]){
-        String[] words = {"cd","ac","dc","ca","zz"};
-        int count = maximumNumberOfStringPairs(words);
-        if(count == 2) System.out.println("True"); //note: static check for this particular array only.
+        int[] nums = {1,13,10,12,31};
+        int count = countDistinctIntegers(nums);
+        if(count == 6) System.out.println("True"); //note: static check for this particular array only.
         else System.out.println("False");
     }
 
-    public static int maximumNumberOfStringPairs(String[] words) {
-        HashSet<String> set = new HashSet<>();
-        int count = 0;
-        for(int i = 0;i < words.length;i++){
-            StringBuilder rev = new StringBuilder(words[i]);
-            String a = rev.reverse().toString();
-            if(set.contains(a)){
-                count++;
-                set.remove(words[i]);
-            } else {
-                set.add(words[i]);
+    public static int countDistinctIntegers(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0;i<nums.length;i++){
+            set.add(nums[i]);
+            int rev = 0;
+            while(nums[i] != 0){
+                rev = rev*10 + nums[i]%10;
+                nums[i] /= 10;  
             }
+            set.add(rev);
         }
-        return count;
+        return set.size();
     }
 }
